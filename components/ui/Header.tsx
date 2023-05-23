ab32eacae74e2f0361862c5489beec7e7dd0bdab
jonarroh <jonarrodi99@gmail.com>
2023-05-23 12:55:08 -0600

    ✨ feat (AUTH) Added auth

---
+++ components/ui/Header.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { SignInButton, useClerk, useUser } from '@clerk/nextjs';
+
+import { Avatar, AvatarFallback, AvatarImage } from './avatar';
+import { Button } from './button';
+
+export default function Header() {
+	const { user, isSignedIn } = useUser();
+	const { signOut } = useClerk();
+
+	return (
+		<div className="flex justify-between items-center">
+			<Avatar>
+				<AvatarImage
+					src={user?.profileImageUrl}
+					alt={user?.firstName ?? 'image'}
+				/>
+				<AvatarFallback className="bg-gray-200 text-slate-950">
+					MD
+				</AvatarFallback>
+			</Avatar>
+
+			{isSignedIn ? (
+				<Button
+					variant={'outline'}
+					onClick={() => {
+						signOut();
+					}}>
+					Logout
+				</Button>
+			) : (
+				<SignInButton mode="modal">
+					<Button variant={'default'}>Login</Button>
+				</SignInButton>
+			)}
+		</div>
+	);
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import Header from '../../components/ui/Header';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -14,17 +16,19 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" className="bg-[#383838]">
-			<body className={inter.className}>
-				<main className="grid grid-cols-12 min-h-screen w-screen text-white">
-					<section className="bg-[#383838] p-12 col-span-12 sm:col-span-12 md:col-span-9">
-						{children}
-					</section>
-					<section className="bg-[#1E1E1E] p-12 hidden sm:hidden sm:col-span-3 md:block md:col-span-3">
-						<p>login</p>
-					</section>
-				</main>
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en" className="bg-[#383838]">
+				<body className={inter.className}>
+					<main className="grid grid-cols-12 min-h-screen w-screen text-white">
+						<section className="bg-[#383838] p-12 col-span-12 sm:col-span-12 md:col-span-9">
+							{children}
+						</section>
+						<section className="bg-[#1E1E1E] p-12 hidden sm:hidden sm:col-span-3 md:block md:col-span-3">
+							<Header />
+						</section>
+					</main>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
